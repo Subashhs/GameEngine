10d0db647279ca838aedbe8529c3591103ef954b
Subashhs <142995902+Subashhs@users.noreply.github.com>
2024-09-24 13:48:10 +0300

    car
    new car

---
+++ Assets/Scripts/GreenTank.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using System.Configuration;
+using System.Collections;
+using UnityEngine;
+
+public class GreenTank : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // public void stings
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //we will move the vehicle
+        transform.Translate(0, 0, 1);
+    }
+}

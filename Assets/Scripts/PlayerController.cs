efee4d7fe008e2e01a1c494beb6888336b5390f8
Subashhs <142995902+Subashhs@users.noreply.github.com>
2024-10-08 16:34:42 +0300

    new

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,48 @@
-using UnityEditor;
-using System.Configuration;
-using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Speed variable to control movement speed
+    public float speed = 5.0f;
+
+    // Rotation speed for turning the vehicle
+    public float rotationSpeed = 100.0f;
+
+    // Reference to the camera
+    public Camera mainCamera;
+
+    // Offset between the camera and the vehicle
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
     void Start()
     {
-        // public void stings
+        // Initialize the camera offset (distance between camera and vehicle)
+        if (mainCamera != null)
+        {
+            cameraOffset = mainCamera.transform.position - transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //we will move the vehicle
-        transform.Translate(0, 0, 1);
+        // Manual vehicle movement with arrow keys or WASD
+        float moveVertical = Input.GetAxis("Vertical"); // Forward/Backward input
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Left/Right input
+
+        // Move the vehicle forward/backward
+        transform.Translate(Vector3.forward * moveVertical * speed * Time.deltaTime);
+
+        // Rotate the vehicle left/right
+        transform.Rotate(Vector3.up, moveHorizontal * rotationSpeed * Time.deltaTime);
+
+        // Camera follows the vehicle
+        if (mainCamera != null)
+        {
+            mainCamera.transform.position = transform.position + cameraOffset;
+            mainCamera.transform.LookAt(transform.position); // Camera always looks at the vehicle
+        }
     }
 }
+ss
